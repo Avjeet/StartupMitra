70ac7ea85c7d38272c77b154166968c2880322ef
hi_man_shoe <hello2himanshusingh@gmail.com>
2017-02-04 00:25:58 +0530

    Tweaked UI

---
+++ app/src/main/java/com/teamshunya/silencio/ShowActivity/ShowActivity.java
@@ -0,0 +1,109 @@
+package com.teamshunya.silencio.ShowActivity;
+
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+import com.teamshunya.silencio.R;
+import com.teamshunya.silencio.ShowActivity.Fragments.Arrival;
+import com.teamshunya.silencio.ShowActivity.Fragments.Departure;
+import com.teamshunya.silencio.ShowActivity.Fragments.Feeback;
+import com.teamshunya.silencio.ShowActivity.Fragments.Offers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(1).select();//sets default fragment as view
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Arrival(), "Arrivals");
+        adapter.addFragment(new Departure(), "Departure");
+        adapter.addFragment(new Offers(), "Offers");
+        adapter.addFragment(new Feeback(), "Feedback");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+
+    }
+
+ @Override
+
+ public boolean onOptionsItemSelected(MenuItem item) {
+
+     switch (item.getItemId()) {
+
+         case R.id.menu_bookmark:
+
+     }
+     return true;
+ }
+
+}
---
+++ app/src/main/java/com/teamshunya/silencio/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.teamshunya.silencio;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.menu.ShowableListMenu;
+
+import com.teamshunya.silencio.ShowActivity.ShowActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashActivity.this, ShowActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
