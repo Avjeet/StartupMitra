91f9f4cc625fc741fa01f959f4553b52d10ce0d9
hi_man_shoe <hello2himanshusingh@gmail.com>
2017-02-05 17:52:16 +0530

    new color

--- app/src/main/java/com/teamshunya/silencio/FeedBack/Add.java
+++ app/src/main/java/com/teamshunya/silencio/FeedBack/Add.java
@@ -3,11 +3,18 @@ package com.teamshunya.silencio.FeedBack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.teamshunya.silencio.R;
 
 public class Add extends AppCompatActivity {
 
+    RatingBar ratingHospitality, ratingAmbience,ratingTransport,ratingLounge,ratingEating;
+    RatingBar ratingClean, ratingBaggage,ratingSecurity;
+    EditText comment,flightNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +23,10 @@ public class Add extends AppCompatActivity {
         //getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setTitle("Feedback");
 
+        ratingHospitality = (RatingBar)findViewById(R.id.ratingHospitality);
+
+//        Toast.makeText(Add.this,
+//                String.valueOf(ratingHospitality.getRating()),
+//                Toast.LENGTH_SHORT).show();
     }
 }
