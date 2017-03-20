f0057bf988d3c867e2e11f351453ed86dee2ca30
hi_man_shoe <hello2himanshusingh@gmail.com>
2017-03-20 20:22:40 +0530

    chk

--- app/src/main/java/com/teamshunya/silencio/ShowActivity/Fragments/Arrival.java
+++ app/src/main/java/com/teamshunya/silencio/ShowActivity/Fragments/Arrival.java
@@ -1,5 +1,6 @@
 package com.teamshunya.silencio.ShowActivity.Fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -30,6 +31,7 @@ import retrofit2.Response;
 public class Arrival extends android.support.v4.app.Fragment {
     private ListView listView;
     private View parentView;
+    private ProgressDialog dialog;
     private List<com.teamshunya.silencio.Models.Arrival> arrivalList;
     private myArrivalAdapter adapter;
 
@@ -40,7 +42,9 @@ public class Arrival extends android.support.v4.app.Fragment {
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        loadArrivalList();
+        final ProgressDialog dialog;
+
+
         super.onActivityCreated(savedInstanceState);
 
     }
@@ -48,7 +52,7 @@ public class Arrival extends android.support.v4.app.Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        loadArrivalList();
+
         return inflater.inflate(R.layout.fragment_arrival, container, false);
 
 
@@ -67,6 +71,7 @@ public class Arrival extends android.support.v4.app.Fragment {
 
 
     private void bindViews(View view) {
+
         loadArrivalList();
         parentView = view.findViewById(R.id.parentLayout);
         listView = (ListView) view.findViewById(R.id.arrival_list);
@@ -75,18 +80,21 @@ public class Arrival extends android.support.v4.app.Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Snackbar.make(parentView,"Flight from "+ arrivalList.get(position).getSource() + " is expected to be here at " + arrivalList.get(position).getEta() + " hrs." + "Collect your baggage from "+ arrivalList.get(position).getGate() +" Counter :)", Snackbar.LENGTH_LONG).show();
+                Snackbar.make(parentView, "Flight from " + arrivalList.get(position).getSource() + " is expected to be here at " + arrivalList.get(position).getEta() + " hrs." + "Collect your baggage from " + arrivalList.get(position).getGate() + " Counter :)", Snackbar.LENGTH_LONG).show();
             }
         });
 
     }
 
     private void loadArrivalList() {
+
         APIInterface api = ApiClient.getApiService();
         Call<ArrivalList> call = api.getFlightDetails();
         call.enqueue(new Callback<ArrivalList>() {
             @Override
             public void onResponse(Call<ArrivalList> call, Response<ArrivalList> response) {
+
+
                 if (response.isSuccessful()) {
                     try {
                         arrivalList = response.body().getArrivals();
@@ -105,4 +113,6 @@ public class Arrival extends android.support.v4.app.Fragment {
 
 
     }
+
+
 }
