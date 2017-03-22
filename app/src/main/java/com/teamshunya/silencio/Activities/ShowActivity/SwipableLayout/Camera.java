ead536a2431383db91d00bfe695c4d5fa24a5ab8
hi_man_shoe <hello2himanshusingh@gmail.com>
2017-03-22 11:23:43 +0530

    commit

--- app/src/main/java/com/teamshunya/silencio/Activities/ShowActivity/SwipableLayout/Camera.java
+++ app/src/main/java/com/teamshunya/silencio/Activities/ShowActivity/SwipableLayout/Camera.java
@@ -1,12 +1,28 @@
 package com.teamshunya.silencio.Activities.ShowActivity.SwipableLayout;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 
 import com.teamshunya.silencio.R;
+
+import java.lang.reflect.Type;
 
 public class Camera extends Fragment {
     public Camera() {
@@ -22,7 +38,7 @@ public class Camera extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        return inflater.inflate(R.layout.fragment_camera, container, false);
     }
 
 }
