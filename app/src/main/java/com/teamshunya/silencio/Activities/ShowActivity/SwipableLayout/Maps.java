d9196f1590c1db1e4583902ec03f7b91f554ece4
hi_man_shoe <hello2himanshusingh@gmail.com>
2017-03-23 12:39:12 +0530

    maps frag

--- app/src/main/java/com/teamshunya/silencio/Activities/ShowActivity/SwipableLayout/Maps.java
+++ app/src/main/java/com/teamshunya/silencio/Activities/ShowActivity/SwipableLayout/Maps.java
@@ -1,14 +1,33 @@
 package com.teamshunya.silencio.Activities.ShowActivity.SwipableLayout;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.teamshunya.silencio.Classes.GPSTracker;
 import com.teamshunya.silencio.R;
 
-public class Maps extends Fragment {
+import java.util.Map;
+
+public class Maps extends Fragment implements OnMapReadyCallback{
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
+    GPSTracker gps;
+    double latitude,longitude;
+    double lat = 12.9218780;
+    double llong =77.6200830;
     public Maps() {
         // Required empty public constructor
     }
@@ -16,13 +35,41 @@ public class Maps extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        gps = new GPSTracker(getContext());
+        if(gps.canGetLocation()){
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();}
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mMapView = (MapView)mView.findViewById(R.id.map);
+        if (mMapView!=null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        mView= inflater.inflate(R.layout.fragment_maps, container, false);
+        return mView;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        MarkerOptions newMarker = new MarkerOptions();
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude,longitude)).title("You are here").snippet("Enjoy your day"));
+        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(latitude,longitude)).zoom(16).bearing(0).tilt(20).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+
+    }
 }
