cf09251e1642d60f9d664fcf7d604d64d6933e32
hi_man_shoe <hello2himanshusingh@gmail.com>
2017-03-26 03:06:14 +0530

    signout

--- app/src/main/java/com/teamshunya/silencio/Activities/ShowActivity/Fragments/Profile.java
+++ app/src/main/java/com/teamshunya/silencio/Activities/ShowActivity/Fragments/Profile.java
@@ -3,6 +3,7 @@ package com.teamshunya.silencio.Activities.ShowActivity.Fragments;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,7 +32,7 @@ public class Profile  extends android.support.v4.app.Fragment  {
     Button Logout;
 
     private FirebaseAuth.AuthStateListener mAuthStateListener;
-
+    FirebaseUser user;
     public Profile() {
 
 
@@ -45,7 +46,7 @@ public class Profile  extends android.support.v4.app.Fragment  {
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
+                 user = firebaseAuth.getCurrentUser();
                 if(user !=null){
                     onSignedInInitialize(user.getDisplayName(),user.getPhotoUrl(),user.getEmail());
                 }
@@ -79,8 +80,8 @@ public class Profile  extends android.support.v4.app.Fragment  {
 
 
     private void onSignedOutCleanup() {
-
     }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -100,11 +101,29 @@ public class Profile  extends android.support.v4.app.Fragment  {
         Logout = (Button) view.findViewById(R.id.logut);
         emailID = (CustomFontTextView) view.findViewById(R.id.emailid);
         imageView = (ImageView) view.findViewById(R.id.imageView);
+
         return view;
     }
 
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFirebaseAuth.signOut();
+            }
+        });
+        super.onActivityCreated(savedInstanceState);
+    }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 
-
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
