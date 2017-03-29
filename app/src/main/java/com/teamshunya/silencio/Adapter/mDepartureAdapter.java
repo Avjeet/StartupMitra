6f6c4e066494f0f18f4d6dbfaa2171677416e0b2
hi_man_shoe <hello2himanshusingh@gmail.com>
2017-03-29 07:14:20 +0530

    departure

--- app/src/main/java/com/teamshunya/silencio/Adapter/mDepartureAdapter.java
+++ app/src/main/java/com/teamshunya/silencio/Adapter/mDepartureAdapter.java
@@ -10,7 +10,6 @@ import android.widget.LinearLayout;
 
 import com.squareup.picasso.Picasso;
 import com.teamshunya.silencio.Classes.CustomFontTextView;
-import com.teamshunya.silencio.Models.Arrival;
 import com.teamshunya.silencio.Models.Departure;
 import com.teamshunya.silencio.R;
 
@@ -51,10 +50,10 @@ public class mDepartureAdapter extends ArrayAdapter<Departure> {
         Departure item = getItem(position);
 
         vh.status_text.setText(item.getDelay() );
-        vh.src_text.setText(item.getSource());
+        vh.destination.setText(item.getDestination());
         vh.flight_text.setText(item.getFlightNo());
         vh.time_text.setText(item.getEta());
-        vh.statuss.setText(item.getDestination());
+        vh.source.setText(item.getSource());
 
         Picasso.with(context).load(item.getLogo()).placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher).into(vh.imageView);
 
@@ -64,20 +63,20 @@ public class mDepartureAdapter extends ArrayAdapter<Departure> {
         public final LinearLayout rootView;
         public final ImageView imageView;
         public final CustomFontTextView status_text;
-        public final CustomFontTextView src_text;
+        public final CustomFontTextView destination;
         public final CustomFontTextView flight_text;
         public final CustomFontTextView time_text;
-        public final CustomFontTextView statuss;
+        public final CustomFontTextView source;
 
 
         private ViewHolder(LinearLayout rootView,ImageView imageView,CustomFontTextView statuss, CustomFontTextView src_text, CustomFontTextView status_text, CustomFontTextView flight_text, CustomFontTextView time_text) {
             this.rootView = rootView;
             this.imageView = imageView;
-            this.src_text = src_text;
+            this.destination = src_text;
             this.flight_text = flight_text;
             this.status_text = status_text;
             this.time_text = time_text;
-            this.statuss = statuss;
+            this.source = statuss;
         }
 
         public static mDepartureAdapter.ViewHolder create(LinearLayout rootView) {
