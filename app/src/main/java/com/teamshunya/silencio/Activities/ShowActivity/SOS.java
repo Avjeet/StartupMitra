036b07056cf9f4fb87aa4a244ac9bdeeb5b3eb40
hi_man_shoe <hello2himanshusingh@gmail.com>
2017-04-02 03:40:36 +0530

    sos

--- app/src/main/java/com/teamshunya/silencio/Activities/ShowActivity/SOS.java
+++ app/src/main/java/com/teamshunya/silencio/Activities/ShowActivity/SOS.java
@@ -3,8 +3,11 @@ package com.teamshunya.silencio.Activities.ShowActivity;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -19,6 +22,7 @@ import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.teamshunya.silencio.Classes.GPSTracker;
+import com.teamshunya.silencio.Classes.MyLocationListener;
 import com.teamshunya.silencio.Database.db;
 import com.teamshunya.silencio.Models.PhoneNumber;
 import com.teamshunya.silencio.R;
@@ -56,24 +60,31 @@ Button ClickMe;
     }
 
     //btnsend
-    public void message(View view) {
-        if (dbhandler.number() == 2) {
+    public void message(View view)
+    {
+        if(dbhandler.number()==2) {
             String phoneNo1 = dbhandler.databaseToPhoneFirst();
             String phoneNo2 = dbhandler.databaseToPhoneSecond();
-
+            Double latitude = 0.0, longitude;
             String message = "Need Your Help. I am in danger.Please Contact me ASAP";
+            LocationManager mlocManager = null;
+            LocationListener mlocListener;
+            mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            mlocListener = new MyLocationListener();
+            mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+            if (mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                latitude = MyLocationListener.latitude;
+                longitude = MyLocationListener.longitude;
+                message = message + "\n My Location is - " + latitude +","+  longitude;
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                if (latitude == 0.0) {
+                    Toast.makeText(getApplicationContext(), "Currently gps has not found your location....", Toast.LENGTH_LONG).show();
+                }
 
-            message = message + "\n My Location is - " + latitude + "," + longitude;
-            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-            if (latitude == 0.0) {
-                Toast.makeText(getApplicationContext(), "Currently gps has not found your location....", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "GPS is currently off...", Toast.LENGTH_LONG).show();
             }
-
-
-            //message sending
-
-
-            try {
+          try {
                 SmsManager smsManager = SmsManager.getDefault();
                 smsManager.sendTextMessage(phoneNo1, null, message, null, null);
                 //Toast.makeText(getApplicationContext(), "SMS1 sent.", Toast.LENGTH_LONG).show();
@@ -90,7 +101,9 @@ Button ClickMe;
                 // Toast.makeText(getApplicationContext(), "SMS2 faild, please try again.", Toast.LENGTH_LONG).show();
                 e.printStackTrace();
             }
-        } else {
+        }
+        else
+        {
             Toast.makeText(getApplicationContext(), "Please add two phone numbers of close ones first.....", Toast.LENGTH_LONG).show();
         }
     }
@@ -99,11 +112,8 @@ Button ClickMe;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sos);
-        gps = new GPSTracker(this);
-        if (gps.canGetLocation()) {
-            latitude = gps.getLatitude();
-            longitude = gps.getLongitude();
-        }
+
+
         ClickMe = (Button)findViewById(R.id.Clickme);
         ClickMe.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/teamshunya/silencio/Classes/MyLocationListener.java
@@ -0,0 +1,40 @@
+package com.teamshunya.silencio.Classes;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by himanshusingh on 02/04/17.
+ */
+
+public class MyLocationListener implements LocationListener {
+
+    public static double latitude;
+    public static double longitude;
+
+    @Override
+    public void onLocationChanged(Location loc)
+    {
+        loc.getLatitude();
+        loc.getLongitude();
+        latitude=loc.getLatitude();
+        longitude=loc.getLongitude();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+        //print "Currently GPS is Disabled";
+    }
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+        //print "GPS got Enabled";
+    }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+
+    }
+}
